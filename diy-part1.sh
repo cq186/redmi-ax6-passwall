4dd284fb351b4cc39b40237295ea90a5ea80da85
检 <752757914@qq.com>
2023-02-12 09:52:50 +0800

    url

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-echo 'src-git op_pkage https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git PWpackages  https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+echo 'src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
+ 
+#echo 'src-git op_pkage https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
